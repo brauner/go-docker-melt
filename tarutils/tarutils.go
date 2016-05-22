b5d04b20126e5beb0226f7ff236ccf2cc488af9e
Christian Brauner <christian.brauner@mailbox.org>
2016-05-22 21:01:08 +0200

    rework tarutils
    Signed-off-by: Christian Brauner <christian.brauner@mailbox.org>

--- tarutils/tarutils.go
+++ tarutils/tarutils.go
@@ -12,24 +12,28 @@ import (
 type Tar interface {
 	// Add directory entry to a tar archive. Handles symbolic links and
 	// device files correctly. The prefix will be stripped from path.
-	AddEntry(w *tar.Writer, path string, prefix string) error
+	CopyTarEntry(w *tar.Writer, path string) (err error)
 
 	// Create a tar archive from a given path. The prefix will be stripped
 	// from path.
-	CreateTar(path string, prefix string) error
+	CreateTarHash(tarball string, path string, prefix string) (checksum []byte, err error)
 
 	// Create a tar archive from a given path and return its sha256
 	// checksum. The prefix will be stripped from path.
-	CreateTarHash(path string, prefix string) error
+	CreateTar(tarball string, path string, prefix string) (err error)
 
 	// Write the tar header for a directory entry to a tar archive. Handles
 	// symbolic links and device files correctly.
-	WriteTarHeader(tw *tar.Writer, statPath string, entry string, fi os.FileInfo) error
+	WriteTarHeader(w *tar.Writer, path string, headerName string, f os.FileInfo) error
 
 	// TODO: Add functions to extract tar archives.
 
 	// Test whether a tar archive is empty.
 	IsEmptyTar(tar string) (bool, error)
+
+	// Takes care to return a correct entry for the Name field in a tar
+	// header struct.
+	TarHeaderEntry(f os.FileInfo, path string, prefix string) (entry string)
 }
 
 func IsEmptyTar(tarball string) (bool, error) {
@@ -38,119 +42,135 @@ func IsEmptyTar(tarball string) (bool, error) {
 		return false, err
 	}
 	defer f.Close()
+
 	t := tar.NewReader(f)
 	_, err = t.Next()
 	if err == io.EOF {
 		return true, nil
 	}
+
 	return false, err
 }
 
-func CreateTarHash(path string, prefix string) ([]byte, error) {
-	f, err := os.Create(path)
+func CreateTarHash(tarball string, path string, prefix string) (checksum []byte, err error) {
+	f, err := os.Create(tarball)
 	if err != nil {
-		return nil, err
+		return
 	}
 	defer f.Close()
 
 	h := sha256.New()
 	mw := io.MultiWriter(h, f)
 	w := tar.NewWriter(mw)
-	defer w.Close()
 
 	err = TarDir(w, path, prefix)
 	if err != nil {
-		return nil, err
+		return
 	}
-	return h.Sum(nil), nil
+
+	if err = w.Close(); err != nil {
+		return
+	}
+
+	checksum = h.Sum(nil)
+	return
 }
 
-func CreateTar(path string, prefix string) error {
-	f, err := os.Create(path)
+func CreateTar(tarball string, path string, prefix string) (err error) {
+	f, err := os.Create(tarball)
 	if err != nil {
-		return err
+		return
 	}
 	defer f.Close()
+
 	w := tar.NewWriter(f)
-	defer w.Close()
-	return TarDir(w, path, prefix)
+
+	err = TarDir(w, path, prefix)
+	if err = w.Close(); err != nil {
+		return
+	}
+
+	return
 }
 
-func WriteTarHeader(tw *tar.Writer, statPath string, entry string, fi os.FileInfo) error {
-	var (
-		link string
-		err  error
-	)
-	if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
-		link, err = os.Readlink(statPath)
+func WriteTarHeader(w *tar.Writer, path string, headerName string, f os.FileInfo) (err error) {
+	var link string
+
+	if f.Mode()&os.ModeSymlink == os.ModeSymlink {
+		link, err = os.Readlink(path)
 		if err != nil {
-			return err
+			return
 		}
 	}
-	header, err := tar.FileInfoHeader(fi, link)
+
+	header, err := tar.FileInfoHeader(f, link)
 	if err != nil {
-		return err
+		return
 	}
-	header.Name = entry
-	if err = tw.WriteHeader(header); err != nil {
-		return err
-	}
-	return nil
+
+	header.Name = headerName
+
+	return w.WriteHeader(header)
 }
 
-func AddEntry(w *tar.Writer, path string, prefix string) error {
-	entry := strings.TrimPrefix(path, prefix)
-	if entry == "" || entry == "/" {
-		return nil
-	}
-	if entry[0:1] == "/" {
-		entry = entry[1:]
-	}
-
-	stat, err := os.Lstat(path)
-	if err != nil {
-		return err
-	}
-
-	mode := stat.Mode()
-	if (mode&os.ModeSymlink == os.ModeSymlink) || (mode&os.ModeDevice == os.ModeDevice) {
-		if err := WriteTarHeader(w, path, entry, stat); err != nil {
-			return err
-		}
-		return nil
-	}
-
-	if err := WriteTarHeader(w, path, entry, stat); err != nil {
-		return err
-	}
-
+func CopyTarEntry(w *tar.Writer, path string) (err error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	if stat.IsDir() {
-		return nil
+		return
 	}
 
-	if _, err := io.Copy(w, f); err != nil {
-		return err
+	if _, err = io.Copy(w, f); err != nil {
+		return
 	}
 
-	return nil
+	if err = f.Close(); err != nil {
+		return
+	}
+
+	return
 }
 
 func TarDir(w *tar.Writer, path string, prefix string) error {
-	return filepath.Walk(path, func(entry string, _ os.FileInfo, err error) error {
+	return filepath.Walk(path, func(entry string, f os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if err := AddEntry(w, entry, prefix); err != nil {
+		s := TarHeaderEntry(f, entry, prefix)
+		if s == "" {
+			return nil
+		}
+
+		mode := f.Mode()
+		if err := WriteTarHeader(w, entry, s, f); err != nil {
+			return err
+		}
+
+		if (mode&os.ModeSymlink == os.ModeSymlink) || (mode&os.ModeDevice == os.ModeDevice) || f.IsDir() {
+			return nil
+		}
+
+		if err := CopyTarEntry(w, entry); err != nil {
 			return err
 		}
 
 		return nil
 	})
+}
+
+func TarHeaderEntry(f os.FileInfo, path string, prefix string) (entry string) {
+	entry = strings.TrimPrefix(path, prefix)
+	if entry == "" || entry == "/" {
+		return
+	}
+
+	if entry[0:1] == "/" {
+		entry = entry[1:]
+	}
+
+	if f.IsDir() && (entry[len(entry)-1:len(entry)] != "/") {
+		entry = entry + "/"
+	}
+
+	return
 }
